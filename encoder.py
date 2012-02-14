18ca170aff8cab7607f6db013a0ade48f2e9d69b
Lucas Kreutz <lucaskalves@gmail.com>
2012-02-14 02:40:19 -0200

    And so, it works

--- encoder.py
+++ encoder.py
@@ -7,17 +7,32 @@ Encodes a file and decodes if a magic world is given
 import sys
 import base64
 
-thefile = open(sys.argv[1], 'rb')
-content = thefile.read()
+
+filename = sys.argv[1]
 
 
 if len(sys.argv) > 2:
   if "moloko" in sys.argv:
-    base64.decode(thefile, thefile)
+    thefile = open(filename, 'r')
+    content = thefile.read()
+    thefile.close()
+    
+    decoded = base64.b64decode(content)
+    thefile = open(filename, 'wb')
+    thefile.write(decoded)
+    thefile.close()
+    
     print("So, that ~secret~ file is decoded... ;)")
   else:
     print("Whoops. '%s' is not a cool word..." % sys.argv[2])
 
 else:
-  base64.encode(thefile, thefile)
+  thefile = open(filename, 'rb')
+  content = thefile.read()
+  thefile.close()
+  
+  encoded = base64.b64encode(content)
+  thefile = open(filename, 'w')
+  thefile.write(encoded)
+  thefile.close()
   print("I encoded that ~secret~ file... :)")
